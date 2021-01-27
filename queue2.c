d04be405afb0e93917f63fbdacf1d27780bc39c4
Will <will0912@icloud.com>
2021-01-27 14:11:33 +0000

    Update queue2.c

--- queue2.c
+++ queue2.c
@@ -15,7 +15,7 @@ typedef struct Node NODE;
 void enqueue(NODE** head_ref, int waitLimit)
 {
     NODE *newNode;
-    if ( ( newNode = (NODE *)malloc(sizeof(NODE)) ) == NULL )
+    if ( ( newNode = (NODE *)malloc(sizeof(NODE)) ) != NULL )
     {
         newNode->waitLimit = waitLimit; 
         newNode->currentWait = 0; 
@@ -24,7 +24,7 @@ void enqueue(NODE** head_ref, int waitLimit)
     }
     else
     {
-        printf("NULL");
+        printf("NOT NULL");
     }
 }
 
