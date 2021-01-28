ebd4c695aed1f589e3c45be2dd79e4f3bfd33af3
Will <will0912@icloud.com>
2021-01-28 21:21:46 +0000

    Update queue2.c

--- queue2.c
+++ queue2.c
@@ -35,7 +35,7 @@ int size(NODE *root)
 { 
     int count = 0;
     NODE *current = root;
-    while (current != NULL) 
+    while (current->next != NULL) 
     { 
         count++; 
         current = current->next; 
