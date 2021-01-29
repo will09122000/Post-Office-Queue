bd3201de0683bfd14950c5c192dc861610bc20f3
Will <will0912@icloud.com>
2021-01-29 11:43:08 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -73,7 +73,7 @@ void runSim()
             }
         }
 
-        print_list(root);
+        /*print_list(root);*/
 
         /* Increment the wait time of all customers in the queue by 1
         updateWait(root); */
