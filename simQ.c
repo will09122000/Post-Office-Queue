00672ae64410e92e7fdbea0aec3360dfc4478a2a
Will <will0912@icloud.com>
2021-01-29 12:28:35 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -74,10 +74,11 @@ void runSim()
         }
 
 
-        /* Increment the wait time of all customers in the queue by 1*/
-        updateWait(root); 
+        
 
         print_list(root);
+        /* Increment the wait time of all customers in the queue by 1*/
+        updateWait(root); 
     }
 
     printf("Size of Queue: %d\n", size(root));
