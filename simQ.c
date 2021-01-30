f93fff25348fd4fcf5c8a9307c188a07dc02ae96
Will <will0912@icloud.com>
2021-01-30 10:51:37 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -87,7 +87,7 @@ void runSim()
                 if (customer) {
                     SERVICEPOINT servicePoint;
                     servicePoint.timeTaken = 0;
-                    servicePoint.timeDone = 5;
+                    servicePoint.timeDone = 8;
                     servicePoint.id = 1;
                     servicePoints[i] = servicePoint;
                 }
@@ -102,7 +102,7 @@ void runSim()
         {
             if (size(customerQueue) < maxQueueLength)
             {
-                int waitLimit = (int)gsl_ran_flat(r,2,3);
+                int waitLimit = (int)gsl_ran_flat(r,3,6);
                 enqueue(customerQueue, waitLimit);
                 customersTotal++;
                 /*printf("Customer Arrived, Wait Limit: %d\n", waitLimit);*/
