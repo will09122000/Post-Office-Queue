6b1a01d1bac809dc5963db6531e52a104564ec3e
Will <will0912@icloud.com>
2021-02-04 13:43:58 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -10,7 +10,7 @@ int main (int argc, char **argv)
     printf("A");
 
     /* Simulation Parameters
-    int simParams[8] = {0,0,0,0,0,0,0}; */
+    int simParams[8] = {0,0,0,0,0,0,0}; 
 
     printf("A");
 
@@ -24,6 +24,7 @@ int main (int argc, char **argv)
     {
         runSim(simParams);
     }
+    */
     return 0;
 }
 
