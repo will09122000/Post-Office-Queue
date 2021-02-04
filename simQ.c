b6c59eab4617a2956a0d0086a16e27a5840bbd27
Will <will0912@icloud.com>
2021-02-04 15:37:38 +0000

    j

--- simQ.c
+++ simQ.c
@@ -22,7 +22,7 @@ int main (int argc, char **argv)
         runSim(simParams, outputLog);
     }
 
-    writeOutputFileOneSim(inputFileName, outputFileName, outputLog);
+    writeOutputFileOneSim(inputFileName, outputFileName);
 
     return 0;
 }
