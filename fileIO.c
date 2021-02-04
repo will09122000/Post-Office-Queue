badb85e862739e6c686fd35ff6c5c7e3373858fa
Will <will0912@icloud.com>
2021-02-04 18:02:50 +0000

    Update fileIO.c

--- fileIO.c
+++ fileIO.c
@@ -69,18 +69,17 @@ void writeLogs(char outputFileName[], int outputLog[], int currentTime, int clos
     int i, j;
     for (i=0; i < currentTime*6; i+=6)
     {
+        if (i/6 == closingTime)
+            fputs("----------- Post Office Closed -----------\n\n", fp);
         fprintf(fp, "Current Time: %d\n", outputLog[i]);
         fprintf(fp, "Customers being Served: %d\n", outputLog[i+1]);
         fprintf(fp, "Customers in Queue: %d\n", outputLog[i+2]);
         fprintf(fp, "Customers Fulfilled: %d\n", outputLog[i+3]);
         fprintf(fp, "Customers Unfulfilled: %d\n", outputLog[i+4]);
         fprintf(fp, "Customers Timed-out: %d\n", outputLog[i+5]);
-        if (i/6 == closingTime)
-            fputs("\n----------- Post Office Closed -----------\n", fp);
         fputs("\n", fp);
     }
     fprintf(fp, "Time taken from closing time until all remaining customers have been served: %d\n", (currentTime-closingTime)-1);
-    printf("%d, %d\n", totalWaitTime, outputLog[(currentTime*6)-3]);
     float avgWaitTime = (float) totalWaitTime / (float) outputLog[(currentTime*6)-3];
     fprintf(fp, "Average Customer Waiting Time: %f\n", avgWaitTime);
 }
