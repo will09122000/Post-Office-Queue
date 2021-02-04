ae5c44b39115964771c01411d71c41d6865175b9
Will <will0912@icloud.com>
2021-02-04 12:14:02 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -203,7 +203,7 @@ void startServingCustomer(int *numServicePoints, SERVICEPOINT servicePoints[], N
             if (customer) {
                 SERVICEPOINT servicePoint;
                 servicePoint.timeTaken = 0;
-                int timeToServe = (int) gsl_ran_rayleigh_tail(&r, 1, 2));
+                int timeToServe = (int) gsl_ran_rayleigh_tail(&r, 1, 2);
                 servicePoint.timeDone = timeToServe;
                 servicePoint.id = 1;
                 servicePoints[i] = servicePoint;
