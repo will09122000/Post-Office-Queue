113d973d603956ffe1a3dbebe5e35e75646a38be
Will <will0912@icloud.com>
2021-02-04 14:14:33 +0000

    Update fileIO.c

--- fileIO.c
+++ fileIO.c
@@ -11,18 +11,19 @@ void getSimParameters(char inputFileName[],
         fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
         exit(1);
     }
-/*
+
     fscanf(fp,
            "maxQueueLength: %d\nnumServicePoints: %d\nclosingTime: %d\nmeanNewCustomers: %d\nlowerLimitWaitTolerance: %d\nupperLimitWaitTolerance: %d\nscaleServeTime: %d\nlowerLimitServeTime: %d\n",
-           test[0],
-           test[1],
-           test[2],
-           test[3],
-           test[4],
-           test[5],
-           test[6],
-           test[7]);
+           &test[0],
+           &test[1],
+           &test[2],
+           &test[3],
+           &test[4],
+           &test[5],
+           &test[6],
+           &test[7]);
 
+/*
 
     int x;
     for(x=0; x<8; x++)
