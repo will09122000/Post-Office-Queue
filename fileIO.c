2f0423aca203ccc58776a5ad396e951c4cdaef3c
Will <will0912@icloud.com>
2021-02-04 17:19:41 +0000

    Update fileIO.c

--- fileIO.c
+++ fileIO.c
@@ -80,6 +80,7 @@ void writeLogs(char outputFileName[], int outputLog[], int currentTime, int clos
         fputs("\n", fp);
     }
     fprintf(fp, "Time taken from closing time until all remaining customers have been served: %d\n", (currentTime-closingTime)-1);
+    printf("%d, &d\n", totalWaitTime, outputLog[(currentTime*6)-3])
     float avgWaitTime = totalWaitTime / outputLog[(currentTime*6)-3];
     fprintf(fp, "Average Customer Waiting Time: %f\n", avgWaitTime);
 }
