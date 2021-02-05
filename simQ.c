d95519fd7ee0ea460d3679945be283b25b0f5e57
Will <will0912@icloud.com>
2021-02-05 21:38:58 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -49,6 +49,7 @@ int main (int argc, char **argv)
         {
             memcpy(avgOutputLog[i], outputLog, sizeof(outputLog));
             totalWaitTime += outputParams.totalWaitTime;
+            printf("%d \n", outputParams.currentTime);
             closingTimes[i] = outputParams.currentTime;
         }
     }
