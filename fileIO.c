a0ea0edeed7423025f05222fbb5887f0dd433286
Will <will0912@icloud.com>
2021-02-05 22:13:29 +0000

    Update fileIO.c

--- fileIO.c
+++ fileIO.c
@@ -122,12 +122,12 @@ void writeLogs(char outputFileName[], int *avgOutputLog, int closingTime, int nu
         exit(1);
     }
 
-    fprintf(fp, "Average Number of Fulfilled Customers:   %f\n", (float) totalcustomersServed / (float) numSims);
-    fprintf(fp, "Average Number of Unfulfilled Customers: %f\n", (float) totalcustomersUnfulfilled / (float) numSims);
-    fprintf(fp, "Average Number of Timed-out Customers:   %f\n", (float) totalcustomersTimedOut / (float) numSims);
+    fprintf(fp, "Average Number of Fulfilled Customers:       %f\n", (float) totalcustomersServed / (float) numSims);
+    fprintf(fp, "Average Number of Unfulfilled Customers:     %f\n", (float) totalcustomersUnfulfilled / (float) numSims);
+    fprintf(fp, "Average Number of Timed-out Customers:       %f\n", (float) totalcustomersTimedOut / (float) numSims);
 
-    fprintf(fp, "Average time taken to serve remaining \n customers after the post office has closed: %f\n", (float) totalTimeAfterClose / (float) numSims);
-    fprintf(fp, "Average Customer Waiting Time: %f\n", (float) totalWaitTime / (float) totalcustomersServed);
+    fprintf(fp, "Average time taken to serve remaining\ncustomers after the post office has closed: %f\n", (float) totalTimeAfterClose / (float) numSims);
+    fprintf(fp, "Average Customer Waiting Time:               %f\n", (float) totalWaitTime / (float) totalcustomersServed);
 
 
 }
