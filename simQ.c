3b9c4746e198decdd7f55863dac3ac39c67d403a
Will <will0912@icloud.com>
2021-02-05 16:32:30 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -39,7 +39,7 @@ int main (int argc, char **argv)
         if (numSims == 1)
             writeLogs(outputFileName, outputLog, outputParams.currentTime, outputParams.closingTime, outputParams.totalWaitTime);
         else
-            avgOutputLog[i] = outputLog;
+            memcpy(avgOutputLog[i], outputLog, sizeof(outputLog));
     }
 
     return 0;
