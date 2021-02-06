c9972bdf22c54a41e9de64c7aad9351de28596f9
Will <will0912@icloud.com>
2021-02-06 13:30:44 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -51,7 +51,7 @@ int main (int argc, char **argv)
         else
         {
             /*memcpy(avgOutputLog[i], outputLog, sizeof(outputLog));*/
-            printf("%d %d %d\n", outputLog[outputParams.currentTime][3], outputLog[outputParams.currentTime][4], outputLog[outputParams.currentTime][5]);
+            printf("%d %d %d\n", outputLog[outputParams.currentTime-1][3], outputLog[outputParams.currentTime-1][4], outputLog[outputParams.currentTime-1][5]);
             avgOutputLog[0] += outputLog[outputParams.currentTime][3];
             avgOutputLog[1] += outputLog[outputParams.currentTime][4];
             avgOutputLog[2] += outputLog[outputParams.currentTime][5];
