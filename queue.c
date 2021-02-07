51fac52e0c33613e14ee35ada612a18f1bf4871c
Will <will0912@icloud.com>
2021-02-07 13:28:20 +0000

    Update queue.c

--- queue.c
+++ queue.c
@@ -73,7 +73,7 @@ NODE dequeue(NODE ** head)
         nodeRemoved = (*head)->next;
         free((*head)->next);
         (*head)->next = NULL;
-        return nodeRemoved;
+        return *nodeRemoved;
 
     default:
         nextNode = (*head)->next->next;
@@ -81,7 +81,7 @@ NODE dequeue(NODE ** head)
         (*head)->next = nodeRemoved->next;
         nextNode->previous = *head;
         free(nodeRemoved);
-        return nodeRemoved;
+        return *nodeRemoved;
     }
 }
 
