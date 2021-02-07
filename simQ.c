df2e96d942252b593ef98eee68da38b0f9b5ec08
Will <will0912@icloud.com>
2021-02-07 12:02:19 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -258,6 +258,7 @@ void startServingCustomer(int numServicePoints, SERVICEPOINT servicePoints[],
                 servicePoint.timeDone = timeToServe;
                 servicePoint.id = 1;
                 servicePoints[i] = servicePoint;
+                printf("%d\n", customer->waitCurrent);
                 *totalWaitTime += customer->waitCurrent;
             }
         }
