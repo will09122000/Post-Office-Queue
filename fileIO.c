af8bac6e0b35a93bfbfdf1e2c9dead07fcac18fb
Will <will0912@icloud.com>
2021-02-09 18:52:47 +0000

    Update fileIO.c

--- fileIO.c
+++ fileIO.c
@@ -30,7 +30,7 @@ void validateInputs(int argc, char **argv)
     /* Check the number of simulations is valid */
     if (atoi(argv[2]) != -1 || atoi(argv[2]) < 1)
     {
-        printf("Number of simulations (second argument) should be an integer
+        printf("Number of simulations (second argument) should be an integer \
 greater than 0 and less than or equal to 2147483647 or equal to -1 if you do \
 not want a maximum queue length. \n");
         error = 1;
