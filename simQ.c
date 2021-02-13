2346128ab4b8e474e3197ade3f276575a87cf32b
Will <will0912@icloud.com>
2021-02-13 13:32:36 +0000

    Update simQ.c

--- simQ.c
+++ simQ.c
@@ -51,7 +51,7 @@ int main (int argc, char **argv)
     printf("Test2\n");
     int j;
 
-    for (i=0; i < outputParams.currentTime; i++)
+    for (i=0; i < 10; i++)
     {
         for(j=0; j < 6; j++)
         {
